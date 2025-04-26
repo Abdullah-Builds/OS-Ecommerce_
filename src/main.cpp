9a4495ed9802c2506900f51b0435f28bd97341c8
Muhammad Abdullah khan <khan.abdullah135790@gmail.com>
2025-04-25 18:17:45 -0700

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,40 +2,6 @@
 #include <pthread.h>
 #include "dining_philosophy.h"
 #include "loader.h"
-#include <unistd.h>
-
-using namespace std;
-
-int main() {
-    Intro();
-
-    pthread_t philosophers[NUM_PHILOSOPHERS];
-    int ids[NUM_PHILOSOPHERS];
-
-    for (int i = 0; i < NUM_PHILOSOPHERS; ++i) {
-        ids[i] = i;
-        if (pthread_create(&philosophers[i], nullptr, philosopher, &ids[i]) != 0) {
-            cout << "Error creating thread for philosopher " << i << endl;
-            return -1;
-        }
-    }
-
-    pthread_mutex_lock(&cv_mutex);
-    current_philosopher = 0;
-    pthread_cond_broadcast(&cv);
-    pthread_mutex_unlock(&cv_mutex);
-
-    for (int i = 0; i < NUM_PHILOSOPHERS; ++i) {
-        pthread_join(philosophers[i], nullptr);
-    }
-
-    cout << "All philosophers have exited. Program ending." << endl;
-    return 0;
-}
-#include <iostream>
-#include <pthread.h>
-#include "dining_philosophy.h"
-#include "loader.h"
 #include<unistd.h>
 
 using namespace std;  
@@ -59,4 +25,3 @@ int main() {
     cout << "All philosophers have exited. Program ending." << endl;
     return 0;
 }
-   
